423324eca2e6504e05dd4b2fc5c03f4709095f57
S.Motoki <sam.moto.com@gmail.com>
2018-12-26 22:08:55 +0900

    [add]deviseの本番環境の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # devise heroku対応設定
+  config.action_mailer.default_url_options = { :host => 'http://dokoiku-ubuntu.herokuapp.com' }
+
 end
