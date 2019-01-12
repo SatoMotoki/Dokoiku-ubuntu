5256d33d2c87ed7725334a5a79fbdbf31a3924c6
S.Motoki <sam.moto.com@gmail.com>
2019-01-12 16:01:04 +0900

    [clean]リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   devise_for :users, only: [:sign_in, :sign_out, :session, :registrations, :omniauth_callback, :confirmation], controllers: { :omniauth_callbacks => "omniauth_callbacks" }
 
+  get '/users', to: redirect("/users/sign_up")
+
   get 'events/manage'
 
   resources :events do
